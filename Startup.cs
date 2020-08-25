483b59419eee2fb760d38c23b278e4f170f2f1bb
sreenuksr <sreenuksr@gmail.com>
2020-08-25 19:20:14 +0530

    Push

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Webgentle.BookStore
 {
@@ -16,6 +17,7 @@ namespace Webgentle.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +28,31 @@ namespace Webgentle.BookStore
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Calling Middleware #1 \n");
+            //    await next();
+            //    await context.Response.WriteAsync("After second Middleware calling middleware #1 \n");
+            //});
+
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Calling Middleware #2 \n");
+            //    await next();
+            //});
+
+
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            //This will execute when URL append with /Sreeni
+            app.UseEndpoints(endPoint =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("SaiRam...!");
-                });
+                endPoint.MapDefaultControllerRoute();
+                //endPoint.MapGet("", async context => { await context.Response.WriteAsync(env.EnvironmentName); });
             });
+
+            //app.UseRouting();
         }
     }
 }
